2af726d1d0ed32bc37b0c7657b791fe2b242572a
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2015-12-22 21:28:13 +0400

    Added dependencies and refactored name of bean

--- web/src/main/java/com/controller/DisplayWordsController.java
+++ web/src/main/java/com/controller/DisplayWordsController.java
@@ -1,25 +1,23 @@
 package com.controller;
 
 import com.controller.base.BaseController;
-import context.DbModuleContext;
 import context.EngineModuleContext;
 import dictionary.training.TrainingUnit;
 import exception.ServiceNotAvailableException;
-import service.TrainingService;
+import service.training.TrainingService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
 public class DisplayWordsController extends BaseController {
     @Override
     protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
         TrainingService trainingService =
-                ( TrainingService ) EngineModuleContext.getAppContext().getBean( "trainingService" );
+                ( TrainingService ) EngineModuleContext.getContext().getBean( "trainingService" );
         try {
             List<TrainingUnit> unitList = trainingService.getAllTrainingUnits();
             req.setAttribute( "units", unitList );
