a108edce25c51632fcf8005c60351ff478ced7cd
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2015-12-30 00:02:03 +0400

    Added pictures, ability to delete words.

---
+++ web/src/main/java/com/controller/DeleteWordsController.java
@@ -0,0 +1,49 @@
+package com.controller;
+
+import com.controller.base.BaseController;
+import context.DbModuleContext;
+import context.EngineModuleContext;
+import dao.training.TrainingDao;
+import dictionary.training.TrainingUnit;
+import exception.ServiceNotAvailableException;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import service.training.TrainingService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class DeleteWordsController extends BaseController {
+    @Override
+    protected void doPost( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
+        String words = req.getParameter( "words" );
+
+        JSONArray wordsArr;
+        try {
+            JSONParser parser = new JSONParser();
+            wordsArr = ( JSONArray ) parser.parse( words );
+        } catch ( ParseException e ) {
+            resp.sendError( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+            return;
+        }
+
+        if ( wordsArr != null && !wordsArr.isEmpty() ) {
+            TrainingService trainingService =
+                    ( TrainingService ) EngineModuleContext.getContext().getBean( "trainingService" );
+
+            for ( Object word : wordsArr ) {
+                String gerWord = ( String ) word;
+                try {
+                    trainingService.deleteUnt( gerWord );
+                } catch ( ServiceNotAvailableException e ) {
+                    resp.sendError( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+                    return;
+                }
+            }
+        }
+    }
+}
--- web/src/main/webapp/static/js/words.js
+++ web/src/main/webapp/static/js/words.js
@@ -4,17 +4,23 @@ $(function () {
         window.location = "/home";
     });
 
-    //$("input[type='text']").focus(function (ev) {
-    //    $(ev.target).removeAttr('placeholder');
-    //});
-
     var toDelete = 0;
 
     // UI for deleting
     $(".pair").click(function (ev) {
         var $target = $(ev.target);
-        if (!$target.hasClass("word")) {
-            //do nothing
+        if (!$target.hasClass("dash") && !$target.hasClass("word")) {
+            // ignore it
+        } else if ($target.hasClass("dash")) {
+            if ($target.hasClass("to-delete-no-line-throw")) {
+                $target.removeClass("to-delete-no-line-throw");
+                $target.siblings(".word").removeClass("to-delete");
+                toDelete--;
+            } else {
+                $target.addClass("to-delete-no-line-throw");
+                $target.siblings(".word").addClass("to-delete");
+                toDelete++;
+            }
         } else {
             if ($target.hasClass("to-delete")) {
                 $target.removeClass("to-delete");
@@ -47,16 +53,38 @@ $(function () {
     $("#add-words-btn").click(function () {
         var data = {
             "russian": $rusField.val(),
-            "german":  $gerField.val()
+            "german": $gerField.val()
         };
         $.ajax({
             url: 'add-words',
             type: 'POST',
             data: data
-        }).always(function() {
+        }).always(function () {
             $rusField.val("");
             $gerField.val("");
             $gerField.focus();
         });
     });
+
+    $(".delete-btn").click(function () {
+        var wordsToDelete = [];
+        $(".to-delete").each(function () {
+            if ($(this).hasClass("ger-word")) {
+                var gerWord = $(this)[0].innerText;
+                wordsToDelete.push(gerWord);
+            }
+        });
+        //alert(wordsToDelete);
+        var data ={
+            "words" : JSON.stringify(wordsToDelete)
+        };
+
+        $.ajax({
+            url: 'delete',
+            type: 'POST',
+            data: data
+        });
+    });
+
+
 });
